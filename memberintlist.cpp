cc461206be035955192c0189b516697c2ed80351
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-21 15:58:07 +0800

    destructor mhs

--- memberintlist.cpp
+++ memberintlist.cpp
@@ -8,5 +8,12 @@ class mahasiswa {
     string nama;
     float nilai;
     public:
-    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai){}
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai){
+    }
+    ~mahasiswa()
+    {
+        cout << "ID     = " << id << endl ;
+        cout << "Nama   = " << nama << endl ;
+        cout << "Nilai  = " << nilai << endl ;
+    }
 };
