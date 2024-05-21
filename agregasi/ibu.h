99fcb346de43c84ba0bef663a31b5e9ad6c8ab68
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-21 16:54:18 +0800

    ibu header

---
+++ agregasi/ibu.h
@@ -0,0 +1,23 @@
+#ifndef IBU_H
+#define IBU_H
+
+class ibu
+{
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama)
+    {
+        cout << "ibu \""<< nama << "\" ada\n";
+    }
+    ibu()
+    {
+        cout << "ibu \""<< nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+void ibu::tambahAnak(anak* pAnak)
+{
+    daftar_anak.push_back(pAnak);
+}
